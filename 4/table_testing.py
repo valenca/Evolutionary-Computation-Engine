a85ff781d388169711a3172c4333a962fdec16fc
Gustavo Martins <gus.a.martins@gmail.com>
2014-03-11 02:25:26 +0000

    plots feitos

--- 4/table_testing.py
+++ 4/table_testing.py
@@ -142,7 +142,7 @@ def jbrandao_phenotype(individual):
 if __name__ == '__main__':
 
 	n_runs = 10
-	n_generations = [500]
+	n_generations = [50,100,500]
 	population_size = [100,250,500]
 	individual_size = [20,50,100]
 	parents_selection_group_size = [3,5,7]
