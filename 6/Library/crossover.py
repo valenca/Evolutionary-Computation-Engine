1eaf48ff69ed3d8a4223a155d33b5ee313640f53
Gustavo Martins <gus.a.martins@gmail.com>
2014-03-18 15:28:27 +0000

    crossover

--- 6/Library/crossover.py
+++ 6/Library/crossover.py
@@ -1,31 +1,50 @@
-from random import sample
+from random import sample, random
 from copy import deepcopy
 
-def cycle_crossover(A,B):
-    count=1
-    length=len(A)
-    mask=[0]*length
-    tog=False
-    for i in A:
-        while(mask[A.index(i)]==0):
-            tog=True
-            mask[A.index(i)]=count
-            i=A[B.index(i)]
-        if tog:
-            tog=False
-            count+=1
-    ab=[A,B]
-    C=[]
-    D=[]
-    for i in range(length):
-        C.append(ab[(mask[i]+1)%2][i])
-        D.append(ab[(mask[i])%2][i])
-    return C,D
-    
-Z=range(10)
-A=sample(Z,len(Z))
-B=sample(Z,len(Z))
-cycle_crossover(A,B)    
+##### Parent Crossover #####
+class Crossover():
+
+	def __init__(self, population_size, individual_size, crossover, crossover_probability):
+		self.population_size = population_size
+		self.individual_size = individual_size
+		self.crossover = crossover
+		self.crossover_probability = crossover_probability
+
+	##### General crossover function #####
+	def crossover(self, parents):
+		offspring = []
+		for i in range(0, len(parents)-1, 2):
+			if random < crossover_probability:
+				offspring1,offspring2 = self.crossover(parents[i]['gen'],parents[i+1]['gen'])
+				offspring.append({'gen':offspring1})
+				offspring.append({'gen':offspring2})
+			else:
+				offspring.append(parents[i])
+				offspring.append(parents[i+1])
+		return offspring
+	######################################
+
+	##### Cycle Crossover #####
+	def crossover_cycle(self, parent1, parent2):
+		count = 1
+		mask = [0]*self.individual_size
+		toggle = False
+		for i in parent1:
+			while mask[parent1.index(i)] == 0:
+				toggle = True
+				mask[parent1.index(i)] = count
+				i = parent1[parent2.index(i)]
+			if toggle:
+				toggle = False
+				count += 1
+		parents = [parent1,parent2]
+		offspring1 = []
+		offspring2 = []
+		for i in range(self.individual_size):
+			offspring1.append(parents[(mask[i]+1)%2][i])
+			offspring2.append(parents[(mask[i]+0)%2][i])
+		return offspring1,offspring2
+	###########################
 
 
 
