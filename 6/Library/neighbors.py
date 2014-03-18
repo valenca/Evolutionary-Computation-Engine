5d0aebdbc304f7f5770bbd57f23225ac85aad4a9
Gustavo Martins <gus.a.martins@gmail.com>
2014-03-18 11:52:34 +0000

    stop

---
+++ 6/Library/neighbors.py
@@ -0,0 +1,6 @@
+##### Neighbors Generator #####
+class Neighbors():
+
+	def __init__(self, population_size, individual_size):
+		self.population_size = population_size
+		self.individual_size = individual_size
