0de09a88f3cb9de92dce9c922e53680a02549124
jsvalenca <jsvalenca@gmail.com>
2014-04-02 00:30:21 +0100

    few identation bugs

--- GPLib/Library/fitness.py
+++ GPLib/Library/fitness.py
@@ -40,6 +40,14 @@ class Fitness():
 	def add_w(x,y): return x + y
 	def mult_w(x,y): return x * y	
 	def sub_w(x,y): return x - y
-	def div_prot_w(x,y): if abs(y) <= 1e-3: return x else: return x / y
-	def exp_w(x,y):	if int(x) == 0: return 0 else: return int(x)**int(y)
+	def div_prot_w(x,y): 
+		if (abs(y) <= 1e-3): 
+			return x 
+		else:
+			return x / y
+	def exp_w(x,y):	
+		if int(x) == 0:
+			return 0 
+		else: 
+			return int(x)**int(y)
 	###################################
