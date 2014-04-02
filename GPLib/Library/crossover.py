934c86098150566e612ad40aef0dd3ac01334b92
Gustavo Martins <gus.a.martins@gmail.com>
2014-04-02 11:10:02 +0100

    ...

--- GPLib/Library/crossover.py
+++ GPLib/Library/crossover.py
@@ -30,22 +30,22 @@ class Crossover():
 		size_2 = self.indiv_size(parent2)
 		cross_point_1 = choice(range(size_1))
 		cross_point_2 = choice(range(size_2))
-		sub_tree_1 = self.sub_tree(par_1, cross_point_1,0)
-		sub_tree_2 = self.sub_tree(par_2, cross_point_2,0)
+		sub_tree_1 = self.sub_tree(parent1, cross_point_1,0)
+		sub_tree_2 = self.sub_tree(parent2, cross_point_2,0)
 		new_par_1 = deepcopy(parent1)
 		offspring = self.replace_sub_tree(new_par_1,sub_tree_1,sub_tree_2)
 		return offspring
 	###################
 
 
-	def indiv_size(indiv):
+	def indiv_size(self, indiv):
 		""" Number of nodes of an individual."""
 		if not isinstance(indiv, list):
 			return 1
 		else:
-			return 1 + sum(map(indiv_size, indiv[1:]))
+			return 1 + sum(map(self.indiv_size(indiv[1:])))
 
-	def sub_tree(tree,position,count):
+	def sub_tree(self, tree, position, count):
 		if position == count:
 			return tree
 		else:
@@ -57,7 +57,7 @@ class Crossover():
 						break
 				return res_aux
 
-	def replace_sub_tree(tree, sub_tree_1, sub_tree_2):
+	def replace_sub_tree(self, tree, sub_tree_1, sub_tree_2):
 		if tree == sub_tree_1: return sub_tree_2
 		elif isinstance(tree, list):
 			for i,sub in enumerate(tree[1:]):
