92feb8d1d0f285375ad05c2591721fc100b53b3e
Gustavo Martins <gus.a.martins@gmail.com>
2014-06-06 12:19:35 +0100

    -files

--- Project_Group/send/Code/Library/test.py
+++
@@ -1,7 +0,0 @@
-from random import sample,choice,randint
-from copy import deepcopy
-
-a=[['a',1],['b',2],['c',3]]
-function.set = [i for i in a if i[0] == 'b' or i[1] != 2]
-print function_set
-print a
